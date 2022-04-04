ed7f792593da3037c208868107965014c46f3361
JonathasGit <jonathas-25@hotmail.com>
2022-04-04 17:36:53 -0300

    Aula de numero 6

--- acoes.js
+++ acoes.js
@@ -37,3 +37,18 @@ function mudarJogador(valor) {
     jogador = valor;
     jogadorSelecionado.innerHTML = jogador;
 }
+
+
+//Verificar se alguém já venceu 
+// Verificar de todas sequencias possiveis de vencer
+function checaVencedor(){
+    var quadrado1 = document.getElementById(elementid = 1);
+    var quadrado2 = document.getElementById(elementid = 2);
+    var quadrado3 = document.getElementById(elementid = 3);
+    var quadrado4 = document.getElementById(elementid = 4);
+    var quadrado5 = document.getElementById(elementid = 5);
+    var quadrado6 = document.getElementById(elementid = 6);
+    var quadrado7 = document.getElementById(elementid = 7);
+    var quadrado8 = document.getElementById(elementid = 8);
+    var quadrado9 = document.getElementById(elementid = 9);
+}
