594ab3fb6505f2a6b0656cb9ba7e8beb6a8e22b9
JonathasGit <jonathas-25@hotmail.com>
2022-04-04 18:35:25 -0300

    todo codigo ok

--- acoes.js
+++ acoes.js
@@ -1,13 +1,21 @@
 var jogador, vencedor = null;
-var jogadorSelecionado = document.getElementById(elementid = jogadorSelecionado);
-var vendcedorSelecionado = document.getElementById(elementid = vendcedorSelecionado);
 
-mudarJogador(valor =  'X');
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+var vencedorSelecionado = document.getElementById('vencedor-selecionado');
+//var jogadorSelecionado = document.getElementById(elementid = jogadorSelecionado);
+//var vencedorSelecionado = document.getElementById(elementid = vendcedorSelecionado);
+
+mudarJogador('X');
 
 
 function escolherQuadrado(id){ //função ao escolher o quadrado 
-    console.log('Clicou no quadrado'); // 
-    console.log(id); // mostrar todos id dos quadrados
+
+           if (vencedor !== null) {
+            return;
+        }
+
+    //console.log('Clicou no quadrado'); // 
+    //console.log(id); // mostrar todos id dos quadrados
 
     var quadrado = document.getElementById(id); // vai receber  elemento HTML 
 
@@ -44,15 +52,15 @@ function mudarJogador(valor) {
 //Verificar se alguém já venceu 
 // Verificar de todas sequencias possiveis de vencer
 function checaVencedor(){
-    var quadrado1 = document.getElementById(elementid = 1);
-    var quadrado2 = document.getElementById(elementid = 2);
-    var quadrado3 = document.getElementById(elementid = 3);
-    var quadrado4 = document.getElementById(elementid = 4);
-    var quadrado5 = document.getElementById(elementid = 5);
-    var quadrado6 = document.getElementById(elementid = 6);
-    var quadrado7 = document.getElementById(elementid = 7);
-    var quadrado8 = document.getElementById(elementid = 8);
-    var quadrado9 = document.getElementById(elementid = 9);
+    var quadrado1 = document.getElementById(1);
+    var quadrado2 = document.getElementById(2);
+    var quadrado3 = document.getElementById(3);
+    var quadrado4 = document.getElementById(4);
+    var quadrado5 = document.getElementById(5);
+    var quadrado6 = document.getElementById(6);
+    var quadrado7 = document.getElementById(7);
+    var quadrado8 = document.getElementById(8);
+    var quadrado9 = document.getElementById(9);
 
     //Verifica  se as sequencias são iguais
     if(checaSequencia(quadrado1, quadrado2, quadrado3)) {
@@ -72,37 +80,72 @@ function checaVencedor(){
     }
     if(checaSequencia(quadrado1, quadrado4, quadrado7)) {
         mudaCorQuadrado(quadrado1, quadrado4, quadrado7);
-        mudarVencedor(quadrado4);
-     
+        mudarVencedor(quadrado1);     
     }
+    if (checaSequencia(quadrado2, quadrado5, quadrado8)) {
+        mudaCorQuadrado(quadrado2, quadrado5, quadrado8);
+        mudarVencedor(quadrado2);
+        return;
+    }
+
+    if (checaSequencia(quadrado3, quadrado6, quadrado9)) {
+        mudaCorQuadrado(quadrado3, quadrado6, quadrado9);
+        mudarVencedor(quadrado3);
+        return;
+    }
+
+    if (checaSequencia(quadrado1, quadrado5, quadrado9)) {
+        mudaCorQuadrado(quadrado1, quadrado5, quadrado9);
+        mudarVencedor(quadrado1);
+        return;
+    }
+
+    if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
+        mudaCorQuadrado(quadrado3, quadrado5, quadrado7);
+        mudarVencedor(quadrado3);
+    }
+
 
 }
 
 //Para mudar o vencedor ali onde mostra o vencedor
 function mudarVencedor(quadrado){
     vencedor = quadrado.innerHTML;
-    vendcedorSelecionado.innerHTML = vencedor;
+    vencedorSelecionado.innerHTML = vencedor;
 
 }
 
 
 // Função para mudar a cor do quadrado, quando o jogador ganhar. 
-function mudaCorQuadrado(quadrado1, quadrado2, quadrado3){
-    quadrado1.style.color = '#00ff00';
-    quadrado2.style.color = '#00ff00';
-    quadrado3.style.color = '#00ff00';
-
-
+function mudaCorQuadrado(quadrado1, quadrado2, quadrado3) {
+    quadrado1.style.background = '#0f0';
+    quadrado2.style.background = '#0f0';
+    quadrado3.style.background = '#0f0';
 }
 
 //Checando a primeira sequencia de vencedor
 function checaSequencia(quadrado1, quadrado2, quadrado3){
 var eigual = false;
 
-if(quadrado1.innerHTML !== '-' && quadrado1.innerHTML === quadrado2.innerHTML && quadrado2.innerHTML === quadrado3.innerHTML){
+if (quadrado1.innerHTML !== '-' && quadrado1.innerHTML === quadrado2.innerHTML && quadrado2.innerHTML === quadrado3.innerHTML) {
     eigual = true;
 }
 
 return eigual;
 
+}
+
+function reiniciar()
+{
+    vencedor = null;
+    vencedorSelecionado.innerHTML = '';
+
+    for (var i = 1; i <= 9; i++) {
+        var quadrado = document.getElementById(i);
+        quadrado.style.background = '#eee';
+        quadrado.style.color = '#eee';
+        quadrado.innerHTML = '-';
+    }
+
+    mudarJogador('X');
 }
