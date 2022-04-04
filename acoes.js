0abc33d172ad56436a6a0cf9f3619b6842b90210
JonathasGit <jonathas-25@hotmail.com>
2022-04-04 17:19:48 -0300

    # parte 5 das aulas

--- acoes.js
+++ acoes.js
@@ -1,8 +1,25 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById(elementid = jogadorSelecionado);
+
+mudarJogador(valor =  'X');
 
 
 function escolherQuadrado(id){ //função ao escolher o quadrado 
     console.log('Clicou no quadrado'); // 
     console.log(id); // mostrar todos id dos quadrados
 
+    var quadrado = document.getElementById(id); // vai receber  elemento HTML 
+    quadrado.innerHTML = jogador; //trocar o texto do html 
+    quadrado.style.color = '#000';
+
+
+
+
+}
+
+//Função que muda o jogador
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
